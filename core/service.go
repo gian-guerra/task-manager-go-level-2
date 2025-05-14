e9d5699ba077e2993077f1978d33eba0aa64d392
gian-guerra <giancguerra0@gmail.com>
2025-05-13 22:56:12 -0500

    include service and ports to manage the use case

--- core/service.go
+++ core/service.go
@@ -1,1 +1,39 @@
 package core
+
+import (
+	"time"
+
+	"github.com/gian-guerra/task-manager-go-level-2/models"
+	"github.com/gian-guerra/task-manager-go-level-2/ports"
+)
+
+type TaskService struct {
+	repository *TaskRepository
+}
+
+var _ ports.TaskServicePort = &TaskService{}
+
+func NewTaskService(repository *TaskRepository) *TaskService {
+	return &TaskService{repository: repository}
+}
+
+func (s *TaskService) AddTask(title string) models.Task {
+	task := models.Task{
+		Title:     title,
+		CreatedAt: time.Now(),
+		Done:      false,
+	}
+	return s.repository.Save(task)
+}
+
+func (s *TaskService) CompleteTask(id string) error {
+	return s.repository.Complete(id)
+}
+
+func (s *TaskService) DeleteTask(id string) error {
+	return s.repository.Delete(id)
+}
+
+func (s *TaskService) ListTasks() []models.Task {
+	return s.repository.GetAll()
+}
